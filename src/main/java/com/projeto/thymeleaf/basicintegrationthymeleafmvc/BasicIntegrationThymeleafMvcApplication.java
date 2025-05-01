3dc6355f066027bc83db7271be8a94d480afa685
EsieleSantos <esielesantos@hotmail.com>
2025-04-30 21:24:57 -0300

    First commit

---
+++ src/main/java/com/projeto/thymeleaf/basicintegrationthymeleafmvc/BasicIntegrationThymeleafMvcApplication.java
@@ -0,0 +1,14 @@
+package com.projeto.thymeleaf.basicintegrationthymeleafmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicIntegrationThymeleafMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicIntegrationThymeleafMvcApplication.class, args);
+	}
+	
+}
+
---
+++ src/test/java/com/projeto/thymeleaf/basicintegrationthymeleafmvc/BasicIntegrationThymeleafMvcApplicationTests.java
@@ -0,0 +1,13 @@
+  package com.projeto.thymeleaf.basicintegrationthymeleafmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicIntegrationThymeleafMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
